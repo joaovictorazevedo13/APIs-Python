dec189b0baca2d659cb5339274c35ce2267a83cf
João Victor Azevedo <109423515+joaovictorazevedo13@users.noreply.github.com>
2022-11-16 14:52:34 -0300

    att

--- CEP.py
+++ CEP.py
@@ -1,18 +1,32 @@
 import fun
 import requests
 
-fun.linha()
-print("Consulta CEP".center(30))
-fun.linha()
+def main():
+    fun.linha()
+    print("Consulta CEP".center(30))
+    fun.linha()
 
-cep_input = input("Digite o CEP: ")
+    cep_input = input("Digite o CEP: ")
 
-if len(cep_input) != 8:
-    print("Quantidade de digitos inválida")
-    exit()
+    if len(cep_input) != 8:
+        print("Quantidade de digitos inválida")
+        exit()
 
-request = requests.get(f"viacep.com.br/ws/{cep_input}/json/")
+    request = requests.get(f"https://viacep.com.br/ws/{cep_input}/json/")
 
-adress_data = request.json()
+    adress_data = request.json()
 
-print(adress_data)
+    if "erro" not in adress_data:
+        if adress_data['logradouro'] == '':
+            adress_data['logradouro'] = 'CEP da Cidade toda'
+            print(f"Rua: {adress_data['logradouro']}")
+        else:
+            print(f"Rua: {adress_data['logradouro']}")
+        print(f"Bairro: {adress_data['bairro']}")
+        print(f"Cidade: {adress_data['localidade']}")
+        print(f"UF: {adress_data['uf']}")
+    else:
+        print("CEP inválido")
+
+if __name__ == '__main__':
+    main()
